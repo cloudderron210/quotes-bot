8ee157e45cfd08af7f79db0ad270815aa8190a7f
cloud210 <shevelevo1993@gmail.com>
2025-02-26 21:47:24 +0500

    async sessionmaker added

--- bot/config.py
+++ bot/config.py
@@ -1,5 +1,8 @@
-
 TOKEN = '7101517441:AAFzmV5NDxLKgvr33zDvew3uncw642kQL10'
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
 
 class BotConfig:
     def __init__(self, admin_ids, welcome_message) -> None:
@@ -7,5 +10,19 @@ class BotConfig:
         self.welcome_message = welcome_message
 
 
+
+
+class Settings(BaseSettings):
+    bot_token: str
+    db_url: str
+    db_echo: bool
         
+    model_config = SettingsConfigDict(
+        env_file = ".env"
+    )
+
+settings = Settings()
+
+
+
 
