daccac59ce88b41a7f869b052886943af293fb3c
cloud210 <shevelevo1993@gmail.com>
2025-02-27 16:31:24 +0500

    default author for new user

--- bot/database/crud.py
+++ bot/database/crud.py
@@ -1,9 +1,24 @@
+from sqlalchemy import update
 from sqlalchemy.ext.asyncio import AsyncSession
+from bot.database.models import Author, User
 from bot.database.session_helper import db_helper
-from bot.database.models import User
 
-async def insert_new_user(user_id, username, session: AsyncSession):
+async def insert_new_user(user_id, username, session: AsyncSession) -> User:
     new_user = User(user_id=user_id, username=username)
     session.add(new_user)
     await session.commit()
     await session.refresh(new_user)
+    return new_user
+
+async def inser_new_author(name: str, session: AsyncSession) -> Author:
+    new_author = Author(name=name)
+    session.add(new_author)
+    await session.commit()
+    await session.refresh(new_author)
+    return new_author
+
+async def set_default_author_for_new(user: User, session: AsyncSession):
+    stmt = update(User).where(User.id == user.id).values(default_author = 2)
+    await session.execute(stmt)
+    await session.commit()
+
