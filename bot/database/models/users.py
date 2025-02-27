14c5cf8f81423400b06ea4de67ec5c7e05bdba42
cloud210 <shevelevo1993@gmail.com>
2025-02-27 15:22:46 +0500

    default author column

--- bot/database/models/users.py
+++ bot/database/models/users.py
@@ -1,4 +1,5 @@
 from typing import TYPE_CHECKING
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from bot.database.models import Base
@@ -11,5 +12,6 @@ class User(Base):
     
     user_id: Mapped[int] = mapped_column(unique=True)
     username: Mapped[str] = mapped_column(nullable=True)
+    default_author: Mapped[int] = mapped_column(ForeignKey('authors.id'), nullable=True)
 
     authors: Mapped[list['Author']] = relationship(secondary='user_author', back_populates='users')
